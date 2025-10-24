af1bccfd9f31ab9335fc6b252cfc4d9e029aafbb
chombatee <128930015+chombatee@users.noreply.github.com>
2025-10-24 19:47:07 +0200

    Add files via upload

---
+++ connect.cs
@@ -0,0 +1,16 @@
+﻿namespace part_1
+{
+    public class connect
+    {
+        public connect()
+        {
+        }
+
+        public string connecting()
+        {
+
+            //then return connection
+            return "Data Source=LabVM1846780\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        }
+    }
+}
